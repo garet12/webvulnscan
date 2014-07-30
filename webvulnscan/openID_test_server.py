005a3151e68d2d78a79c9c641b75bae92f4d2b87
garet12 <Garet12@googlemail.com>
2014-07-31 00:51:16 +0200

    Content-Type Header added

--- webvulnscan/openID_test_server.py
+++ webvulnscan/openID_test_server.py
@@ -22,6 +22,7 @@ class Handler(BaseHTTPRequestHandler):
 
     def _default_page(self, pageState="serverxml"):
         self.send_response(200)
+        self.send_header('Content-type','text/html')
         self.end_headers()
         self.wfile.write('''
         <!DOCTYPE html>
